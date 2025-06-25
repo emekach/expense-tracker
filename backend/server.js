9a2f8682ef0f30a496bbe34cd132e782cabb9e2e
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-06-25 11:32:18 +0100

    staging files

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialised succefully");
+  } catch (error) {
+    console.log(`Error initialising DB ${error}`);
+    process.exit(1);
+  }
+}
+
+app.get("/", (req, res) => {
+  res.send("Its working 123");
+});
+
+initDB().then(() => {
+  app.listen(port, () => {
+    console.log(`server is up and running on PORT:${port}`);
+  });
+});
