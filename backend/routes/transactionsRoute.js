e97243d41d46bd5c66123ede55a34bd66604ef12
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-06-28 22:56:57 +0100

    refactor: reorganise the logic in proper folders like controllers, routes, middleware

---
+++ backend/routes/transactionsRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import {
+  getTransactionsByUserId,
+  createTransaction,
+  deleteTransaction,
+  getSummaryByUserId,
+} from "../controller/transactionsController.js";
+const router = express.Router();
+
+router.get("/:userId", getTransactionsByUserId);
+
+router.post("/", createTransaction);
+
+router.delete("/:id", deleteTransaction);
+
+router.get("/summary/:userId", getSummaryByUserId);
+export default router;
